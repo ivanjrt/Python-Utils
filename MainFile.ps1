c1a9eebba23f03785dae026d8e5fde0472341890
ivanjrt <44326428+ivanjrt@users.noreply.github.com>
2020-12-01 20:44:30 -0600

    Update MainFile.ps1

--- MainFile.ps1
+++ MainFile.ps1
@@ -2,28 +2,29 @@
 # which at the moment don't have the time to make it state of the art but feel free to help if you want to
 
 #Install VSC
-write-output "Installing Visual Studio Code"
+write-output "Installing Visual Studio Code..."
 	if (!(Test-Path -Path c:\temp)) {md c:\temp}
 	$output = "C:\Temp"
 	iwr -Uri https://go.microsoft.com/fwlink/?Linkid=852157 -UseBasicParsing -OutFile $output\VSC_CODE.EXE
 	Start-Process -Wait -FilePath $output\VSC_CODE.EXE -Argument "/VERYSILENT /MERGETASKS=!runcode"
 
 #Install Ptython
-write-output "Installing Python 3.9"
+write-output "Installing Python 3.9..."
 	iwr -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" -UseBasicParsing -OutFile $output\Python.EXE
 	Start-Process -Wait -FilePath $output\Python.EXE -Argument "/quiet InstallAllUsers=1 PrependPath=1"
 	
 	#Warning!!!. ScreenShot your Windows "ENVIROMEENT VARIABLES" location: USER > PATH, then apply the below
 	#so if anything goes bad, then you know what to put back
 	#or if you prefer do it manually, just add the path "c:\.... as shown below"
-write-output ""
+write-output "Adding Pythong Variable Paths..."
 	setx PATH "%PATH%;C:\Program Files\Python39\"
 	setx PATH "%PATH%;C:\Program Files\Python39\Scripts"
 	setx path "%path%;C:\Program Files\Microsoft VS Code"
 	
 	#RESTART YOUR CONSOLE
 	
-#Install Python Ext in VSC
+#adding exts Python Ext in VSC
+write-output "Adding Extensions to VSC..."
 	cd "C:\Program Files\Microsoft VS Code\bin"
 	.\code --install-extension ms-python.python
 
@@ -51,12 +52,14 @@ write-output ""
   #
 		
 #To Upgrade your PIP
+write-output "Upgrading PIP..."
 	python -m pip install --upgrade pip
 	#at first you will get a warning but if you do it again
 	python.exe -m pip install --upgrade pip
 	#warning should dissappear
 	
 #Bonus: PowerShell 7
+write-output "Installing PowerShell 7..."
 iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
 
 	
